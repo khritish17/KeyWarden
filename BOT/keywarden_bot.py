10125af1b8518e05e99aa4039b85f6d193aa3031
Khritish Kumar Behera <khritish.official@gmail.com>
2023-11-30 22:18:16 +0530

    Telebot is used instead of Python-Telegram-Bot

--- BOT/keywarden_bot.py
+++ BOT/keywarden_bot.py
@@ -1,39 +1,60 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telebot
+from telebot import types
 from dotenv import load_dotenv
 import os
-# from ..authentication import login as LG
 
-# accessing the token
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("KEYWARDEN_TOKEN")
 
-# setting up the bot
-updater = Updater(TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+# create the bot
+bot = telebot.TeleBot(TOKEN)
+# user_data
+user_data = {'query': None, 'login_UID': None, 'login_PWD':None, 'signup_UID': None, 'signup_PWD': None, 'mob_no':None}
 
-# get update from telegram bot: KeyWarden 
-updater.start_polling()
+# start function
+@bot.message_handler(commands=['start'])
+def start(message):
+    msg = bot.send_message(message.chat.id, "Hi this is KeyWarden Bot!!!")
+    # bot.register_next_step_handler(msg, ask_login_signup)
+    ask_login_signup(message)
 
-# bot functions
-def start(update, context):
-    update.message.reply_text('Hello welcome to the login/signUp interface of KeyWarden: wielding security for your keys')
-dispatcher.add_handler(CommandHandler("start", start))
+# ask whether for login or signup
+def ask_login_signup(message):
+    msg = bot.send_message(message.chat.id, "Enter:\n1 to login\n2 to signup")
+    bot.register_next_step_handler(msg, get_login_signup)
+# get the response for login or signup
+def get_login_signup(message):
+    user_data['query'] = message.text
+    # bot.register_next_step_handler(message, decide_login_signup)
+    decide_login_signup(message)
 
-def login(update, context):
-    prev_reply = update.message.text
-    update.message.reply_text('LoginID')
-    UID = None
-    while True:
-        print(f"{prev_reply} -> {update.message.text}")
-        new_reply = update.message.text
-        if str(prev_reply) != str(new_reply):
-            UID = update.message.text
-            break
-    print(UID)
-    # context.bot.send_message(update.message.chat_id, 'Please enter your Password')
-dispatcher.add_handler(CommandHandler("login", login))
+# decide whether login or signup based on given query
+# query = 1-> login
+# query = 2 -> signup
+
+def decide_login_signup(message):
+    if user_data['query'] == "1":
+        ask_login_UID(message)
+    elif user_data['query'] == "2":
+        print("Signup")
+    else:
+        ask_login_signup(message)
+
+# login interface
+def ask_login_UID(message):
+    msg = bot.send_message(message.chat.id, "Enter your login ID")
+    bot.register_next_step_handler(msg, get_login_UID)
+def get_login_UID(message):
+    user_data['login_UID'] = message.text
+    ask_login_PWD(message)
+def ask_login_PWD(message):
+    msg = bot.send_message(message.chat.id, "Enter the password")
+    bot.register_next_step_handler(msg, get_login_PWD)
+def get_login_PWD(message):
+    user_data['login_PWD'] = message.text
+    print(f"LoginID:{user_data['login_UID']}, PWD:{user_data['login_PWD']}")
 
 
 
-# continuously runs, unless interrupted and listens for updates
-updater.idle()
+if __name__ == '__main__':
+    bot.polling()
