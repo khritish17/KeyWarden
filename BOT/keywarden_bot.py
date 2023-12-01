2f20bab6164262b0cd7a402ef9c723cfeff8fdd7
Khritish Kumar Behera <khritish.official@gmail.com>
2023-12-01 23:51:23 +0530

    KeyWarden Telegram bot, login and signup interface is up

--- BOT/keywarden_bot.py
+++ BOT/keywarden_bot.py
@@ -15,46 +15,71 @@ user_data = {'query': None, 'login_UID': None, 'login_PWD':None, 'signup_UID': N
 @bot.message_handler(commands=['start'])
 def start(message):
     msg = bot.send_message(message.chat.id, "Hi this is KeyWarden Bot!!!")
-    # bot.register_next_step_handler(msg, ask_login_signup)
-    ask_login_signup(message)
-
-# ask whether for login or signup
-def ask_login_signup(message):
-    msg = bot.send_message(message.chat.id, "Enter:\n1 to login\n2 to signup")
-    bot.register_next_step_handler(msg, get_login_signup)
-# get the response for login or signup
-def get_login_signup(message):
-    user_data['query'] = message.text
-    # bot.register_next_step_handler(message, decide_login_signup)
-    decide_login_signup(message)
-
-# decide whether login or signup based on given query
-# query = 1-> login
-# query = 2 -> signup
-
-def decide_login_signup(message):
-    if user_data['query'] == "1":
-        ask_login_UID(message)
-    elif user_data['query'] == "2":
-        print("Signup")
-    else:
-        ask_login_signup(message)
+    login_signup(msg)
+# asks user whether to login or signup
+def login_signup(message):
+    markup = types.InlineKeyboardMarkup()
+    login_button = types.InlineKeyboardButton("LOGIN", callback_data = "login")
+    signup_button = types.InlineKeyboardButton("SIGNUP", callback_data = "signup")
+    markup.add(login_button, signup_button)
+    bot.send_message(message.chat.id, "Would you like to:", reply_markup=markup)
+# querry handler to decide to proceed with login/signup based on users reply
+@bot.callback_query_handler(func=lambda call: True)
+def login_signup_decide(call):
+    if call.data == "login":
+        user_data["query"] = "login"
+        bot.send_message(call.message.chat.id, "Please enter your details for the login procedure")
+        ask_login_UID(call.message)
+    if call.data == "signup":
+        user_data["query"] = "signup"
+        bot.send_message(call.message.chat.id, "Kindly input your details for the signup procedure")
+        ask_signup_UID(call.message)
+    bot.delete_message(call.message.chat.id, call.message.message_id)
+    bot.delete_message(call.message.chat.id, call.message.message_id -1 )
 
 # login interface
 def ask_login_UID(message):
-    msg = bot.send_message(message.chat.id, "Enter your login ID")
+    msg = bot.send_message(message.chat.id, "Please provide your login ID")
     bot.register_next_step_handler(msg, get_login_UID)
 def get_login_UID(message):
     user_data['login_UID'] = message.text
     ask_login_PWD(message)
 def ask_login_PWD(message):
-    msg = bot.send_message(message.chat.id, "Enter the password")
+    msg = bot.send_message(message.chat.id, "Kindly input the password")
     bot.register_next_step_handler(msg, get_login_PWD)
 def get_login_PWD(message):
     user_data['login_PWD'] = message.text
-    print(f"LoginID:{user_data['login_UID']}, PWD:{user_data['login_PWD']}")
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.delete_message(message.chat.id, message.message_id -1)
+    bot.delete_message(message.chat.id, message.message_id -2)
+    bot.delete_message(message.chat.id, message.message_id -3)
+    bot.delete_message(message.chat.id, message.message_id -4)
 
-
+# signup interfarce
+def ask_signup_UID(message):
+    msg = bot.send_message(message.chat.id, "Please supply a unique user ID; it is strongly advised to utilize your Telegram mobile number")
+    bot.register_next_step_handler(msg, get_signup_UID)
+def get_signup_UID(message):
+    user_data['signup_UID'] = message.text
+    ask_signup_PWD(message)
+def ask_signup_PWD(message):
+    msg = bot.send_message(message.chat.id, "Please enter a password, ensuring to commit it to memory as recovery is not possible")
+    bot.register_next_step_handler(msg, get_signup_PWD)
+def get_signup_PWD(message):
+    user_data["signup_PWD"] = message.text
+    ask_signup_MOB(message)
+def ask_signup_MOB(message):
+    msg = bot.send_message(message.chat.id, "Kindly input your Telegram mobile number for receiving OTP and password in future")
+    bot.register_next_step_handler(msg, get_signup_MOB)
+def get_signup_MOB(message):
+    user_data["mob_no"] = message.text
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.delete_message(message.chat.id, message.message_id - 1)
+    bot.delete_message(message.chat.id, message.message_id - 2)
+    bot.delete_message(message.chat.id, message.message_id - 3)
+    bot.delete_message(message.chat.id, message.message_id - 4)
+    bot.delete_message(message.chat.id, message.message_id - 5)
+    bot.delete_message(message.chat.id, message.message_id - 6)
 
 if __name__ == '__main__':
     bot.polling()
